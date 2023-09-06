33436bea2e081480d564ba56ee06d4ee8c5cee52
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2023-09-06 11:59:43 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,24 @@ function App() {
           </div>
         </button>
       </header>
+      <div className='content-container'>
+        <div className='heading-container'>
+          <h2>Lets grow your</h2>
+          <h2> social presence.</h2>
+        </div>
+        <div className='bubble target'>
+        🎯
+        </div>
+        <div className='bubble shop'>
+        🛍️
+        </div>
+        <div className='bubble rocket'>
+        🚀
+        </div>
+        <div className='bubble fire'>
+        🔥
+        </div>
+      </div>
     </main>
   )
 }
